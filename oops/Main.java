3cc17042aa4b7548f93940ec3b82ea34acf0f40c
indukuri Gowtham Kishore <indukurigowthamkishore@indukuris-MacBook-Air.local>
2024-12-19 23:21:26 +0530

    added new file

---
+++ oops/Main.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class Main{
+    public static void main(String[] args) {
+       System.out.println(GetSum(1,2));
+    }
+       
+    public static int GetSum(int a, int b) {
+        return a + b;
+    }
+}
